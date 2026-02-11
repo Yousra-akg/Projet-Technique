1dcb612fec9910c50ec1c1e6c84023d08f2b45f0
Yousra-akg <akagouyoussra@gmail.com>
2026-02-11 13:29:07 +0100

    Add admin routes to admin branch

--- Project-Management/routes/web.php
+++ Project-Management/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicTaskController;
+use App\Http\Controllers\TaskController;
 
+// Public Routes
 Route::get('/', [PublicTaskController::class, 'index'])->name('home');
 Route::get('/details/{task}', [PublicTaskController::class, 'show'])->name('public.tasks.show');
+
+// Admin Routes
+Route::resource('tasks', TaskController::class);
+Route::get('/admin', [TaskController::class, 'index'])->name('admin.index');
